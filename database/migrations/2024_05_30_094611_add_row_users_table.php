a88854f706b3bb99e6b97c2c0c5b84a976f380e5
Stefan <stefan@studiokhi.com>
2024-06-04 13:54:38 +0200

    fix: migration users table

--- database/migrations/2024_05_30_094611_add_row_users_table.php
+++ database/migrations/2024_05_30_094611_add_row_users_table.php
@@ -11,15 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+        Schema::table('users', function (Blueprint $table) {
             $table->string('phone')->nullable();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
         });
     }
 
