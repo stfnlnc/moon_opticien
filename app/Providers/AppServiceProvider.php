c4394eaa3c4a44033bd0788c394e4350922f75b7
Stefan <stefan@studiokhi.com>
2024-06-11 15:58:47 +0200

    fix: redirect assets https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Models\Option;
+use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\Facades\Vite;
@@ -16,14 +17,20 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        if(env('REDIRECT_HTTPS')) {
+            $this->app['request']->server->set('HTTPS', true);
+        }
     }
 
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot(UrlGenerator $url): void
     {
+        if(env('REDIRECT_HTTPS')) {
+            $url->formatScheme('https');
+        }
+
         $option = Option::first();
         View::share('option', $option);
     }
