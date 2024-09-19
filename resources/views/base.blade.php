4f6fe749366b9f13ae39319ff6f17891b17ab96a
Stefan <stefan@studiokhi.com>
2024-09-19 15:28:19 +0200

    feat: google analytics

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -1,6 +1,15 @@
 <!doctype html>
 <html lang="fr">
 <head>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-GD3L0LG496"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-GD3L0LG496');
+    </script>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=5.0, minimum-scale=1.0">
